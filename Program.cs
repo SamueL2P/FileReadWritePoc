d7f8438b6bee9b299b4735ab68fea9753c844d75
Samuel <padmadansamuel@gmail.com>
2024-07-22 14:43:57 +0530

    functions

--- Program.cs
+++ Program.cs
@@ -3,63 +3,71 @@
     internal class Program
     {
         static void Main(string[] args)
-        {   
+        {
 
             //Read Write HTML file with File class
 
             string htmlFilePath = @"D:\C#programs\FileReadWritePoc\bin\Debug\net8.0\Text File\Text.html";
             string defaultHtmlContent = "<html><body><h1>Welcome to Samuel's HTML File</h1></body></html>";
 
-            File.WriteAllText(htmlFilePath, defaultHtmlContent);
-            Console.WriteLine("Html content written");
-            if (File.Exists(htmlFilePath))
-            {
-                string htmlContent = File.ReadAllText(htmlFilePath);
-                Console.Write("\nHTML File Content:");
-                Console.WriteLine(htmlContent);
-            }
-            else
-            {
-                Console.WriteLine("HTML file not found.");
-            }
+            WriteFile(htmlFilePath, defaultHtmlContent);
+            ReadFile(htmlFilePath);
+            
 
             //Read Write Text File
 
             string textFilePath = @"D:\C#programs\FileReadWritePoc\bin\Debug\net8.0\Text File\Text.txt";
             string textContent = "Welcome to Samuel's Text File";
 
-            File.WriteAllText(textFilePath, textContent);
-            Console.WriteLine("\nText content written");
-            if (File.Exists(textFilePath))
-            {
-                string textReadContent = File.ReadAllText(textFilePath);
-                Console.Write("\nText File Content:");
-                Console.WriteLine(textReadContent);
-            }
-            else
-            {
-                Console.WriteLine("Text file not found.");
-            }
+            WriteFile(textFilePath, textContent);
+            ReadFile(textFilePath);
 
 
             //Append username 
 
             string filePath = @"D:\C#programs\FileReadWritePoc\bin\Debug\net8.0\Text File\Append.txt";
+            AppendUsername(filePath);
 
+           
+
+        }
+
+        static void WriteFile(string path , string content)
+        {
+
+            File.WriteAllText(path, content);
+            Console.WriteLine("\nContent written");
+        }
+
+        static void ReadFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                string content = File.ReadAllText(path);
+                Console.Write("\nHTML File Content:");
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
+        }
+
+        static void AppendUsername(string path)
+        {
             Console.Write("\nEnter your username: ");
             string username = Console.ReadLine();
 
-            File.AppendAllText(filePath, username + Environment.NewLine);
+            File.AppendAllText(path, username + Environment.NewLine);
 
             Console.WriteLine("Username has been saved.");
 
             Console.WriteLine("All usernames in the file:");
-            string[] allUsernames = File.ReadAllLines(filePath);
+            string[] allUsernames = File.ReadAllLines(path);
             foreach (string user in allUsernames)
             {
                 Console.WriteLine(user);
             }
-
         }
     }
 }
